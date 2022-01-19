6957a61cdcdbbd19e51ec9dbb7cab1f1c7fe1682
Cameron Chardukian <camchardukian@gmail.com>
2022-01-18 21:56:08 -0600

    feature/guessed-words

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -1,7 +1,38 @@
 import PropTypes from "prop-types";
 
 const GuessedWords = (props) => {
-  return <div />;
+  const hasGuessedWords = props.guessedWords.length > 0;
+  return (
+    <div data-test="component-guessed-words">
+      {hasGuessedWords ? (
+        <div data-test="guessed-words">
+          <h3>Guessed Words</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>Guess</th>
+                <th>Matching Letters</th>
+              </tr>
+            </thead>
+            <tbody>
+              {props.guessedWords.map((word, index) => {
+                return (
+                  <tr data-test="guessed-word" key={index}>
+                    <td>{word.guessedWord}</td>
+                    <td>{word.letterMatchCount}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <span data-test="guess-instructions">
+          Try to guess the secret word!
+        </span>
+      )}
+    </div>
+  );
 };
 
 GuessedWords.propTypes = {
