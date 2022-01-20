a5d4d40a9796ad5a9e37b6a3d4aa49f82f3422d3
Cameron Chardukian <camchardukian@gmail.com>
2022-01-20 01:08:44 -0600

    feature/input-success-functionality-and-tests

--- src/Input.test.js
+++ src/Input.test.js
@@ -3,18 +3,53 @@ import React from "react";
 import { findByTestAttr, checkProps } from "../test/testUtils";
 import Input from "./Input";
 
-const setup = (secretWord = "hello") => {
-  return shallow(<Input secretWord={secretWord} />);
+const setup = (success = false, secretWord = "hello") => {
+  return shallow(<Input success={success} secretWord={secretWord} />);
 };
+
+describe("render", () => {
+  describe("success is true", () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = setup(true);
+    });
+    test("input renders without error", () => {
+      const wrapper = setup({ secretWord: "train" });
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
+    });
+    test("input box does not show", () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.exists()).toBe(false);
+    });
+    test("submit button does not show", () => {
+      const submitBtn = findByTestAttr(wrapper, "submit-button");
+      expect(submitBtn.exists()).toBe(false);
+    });
+  });
+  describe("success is false", () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = setup(false);
+    });
+    test("input renders without error", () => {
+      const wrapper = setup({ secretWord: "train" });
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
+    });
+    test("input box shows", () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.exists()).toBe(true);
+    });
+    test("submit button shows", () => {
+      const submitBtn = findByTestAttr(wrapper, "submit-button");
+      expect(submitBtn.exists()).toBe(true);
+    });
+  });
+});
 
 test("does not throw warning with expected props", () => {
   checkProps(Input, { secretWord: "hello" });
-});
-
-test("renders without error", () => {
-  const wrapper = setup({ secretWord: "train" });
-  const component = findByTestAttr(wrapper, "input-component");
-  expect(component.length).toBe(1);
 });
 
 describe("state controlled input field", () => {
